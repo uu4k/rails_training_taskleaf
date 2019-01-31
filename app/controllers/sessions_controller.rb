88419337c881f6c5cc46b8bbdb153f795cec444a
uu4k <t.shimizu1115@gmail.com>
2019-01-31 11:32:00 +0900

    add logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action: :login_required
+
   def new
   end
 
@@ -11,6 +13,11 @@ class SessionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
   end
 
   private
