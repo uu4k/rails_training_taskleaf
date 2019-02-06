f922e71a81f11b03d0e909e2dbda14730ba4d551
uu4k <t.shimizu1115@gmail.com>
2019-02-06 10:01:33 +0900

    add confirm and search.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).recent
   end
 
   def show
@@ -11,8 +12,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,18 @@
 class Task < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
 
   belongs_to :user
 
   scope :recent, -> {
-    order(created_at: :dest)
+    order(created_at: :desc)
   }
 end
